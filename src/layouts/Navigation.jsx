d1a48aeca21a1f5698f96c65d94f0bdd063a1c64
Varsha Rana <varsharana2207@outlook.com>
2023-07-11 17:58:02 +0200

    failed committed changes repush

--- src/layouts/Navigation.jsx
+++ src/layouts/Navigation.jsx
@@ -8,30 +8,30 @@ export const Navigation = () => {
       id: 1,
       isExpanded: true,
       subOptions: [
-        { id: 10, label: "Home Level 1", href: "https://condenast.com" },
-        { id: 20, label: "Home Level 2", href: "https://condenast.com" },
-        { id: 30, label: "Home Level 3", href: "https://condenast.com" },
-      ],
+        { id: 10, label: "Home Level 1" },
+        { id: 20, label: "Home Level 2" },
+        { id: 30, label: "Home Level 3" }
+      ]
     },
     {
       label: "About",
       id: 2,
       isExpanded: false,
       subOptions: [
-        { id: 1, label: "About Level 1", href: "https://condenast.com" },
-        { id: 2, label: "About Level 2", href: "https://condenast.com" },
-        { id: 3, label: "About Level 3", href: "https://condenast.com" },
-      ],
-    },
+        { id: 1, label: "About Level 1" },
+        { id: 2, label: "About Level 2" },
+        { id: 3, label: "About Level 3" }
+      ]
+    }
   ]);
 
   const toggleFirstLevel = (e, value) => {
     e.preventDefault();
     const targetTagName = e.target.tagName.toLowerCase();
     if (targetTagName !== "a") {
-      console.log(`toggle first level`, e.target, value);
-      const updatedOptions = navOptions.map((item) =>
-        value === item.id ? { ...item, isExpanded: !item.isExpanded } : item
+      const updatedOptions = navOptions.map(
+        item =>
+          value === item.id ? { ...item, isExpanded: !item.isExpanded } : item
       );
 
       setNavOptions(updatedOptions);
@@ -43,22 +43,23 @@ export const Navigation = () => {
       <nav>
         <ul>
           {navOptions.map((option, index) => (
-            <li key={index} onClick={(e) => toggleFirstLevel(e, option.id)}>
-              <span href={option.href}>
+            <li key={index} onClick={e => toggleFirstLevel(e, option.id)}>
+              <span>
                 {/* Used conditional operator to show either "-" or "+" based on isExpanded */}
                 {option.label} {option.isExpanded ? "-" : "+"}
               </span>
               {/* Used logical AND operator to conditionally render the subOptions */}
-              {option.subOptions && option.isExpanded && (
-                <ul>
-                  {option.subOptions.map((subOption, subIndex) => (
-                    <li key={subIndex}>
-                      {/* Replaced anchor tag with Link component */}
-                      <Link to={subOption.href}>{subOption.label}</Link>
-                    </li>
-                  ))}
-                </ul>
-              )}
+              {option.subOptions &&
+                option.isExpanded && (
+                  <ul>
+                    {option.subOptions.map((subOption, subIndex) => (
+                      <li key={subIndex}>
+                        {/* Replaced anchor tag with Link component */}
+                        <span>{subOption.label}</span>
+                      </li>
+                    ))}
+                  </ul>
+                )}
             </li>
           ))}
         </ul>
